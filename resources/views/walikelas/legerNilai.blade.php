f7ee735ae5ab69d0836acdc20636d8913eee52be
harimiha <139672885+harimiha@users.noreply.github.com>
2024-11-25 00:54:38 +0700

    leger nilai updated

--- resources/views/walikelas/legerNilai.blade.php
+++ resources/views/walikelas/legerNilai.blade.php
@@ -1,6 +1,19 @@
 @extends('layout.layout')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/5.0.4/css/fixedColumns.dataTables.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
+<style>
+    th,
+    td {
+        white-space: nowrap;
+    }
+
+    div.dataTables_wrapper {
+        width: 800px;
+        margin: 0 auto;
+    }
+</style>
 @section('content')
 <div class="container-fluid mt-3">
     <div class="card mb-3 border-0 shadow-sm" style="background-color:#f2f2f2;">
@@ -9,15 +22,22 @@
         </div>
     </div>
 
-    <table id="example" class="table table-striped" style="width:100%">
+    <table id="example" class="table table-striped stripe row-border order-column" style="width:100%">
         <thead>
             <tr>
                 <th class="text-start">No</th>
+                <th class="text-start">Nama</th>
                 <th>Nama</th>
-                <th>Kelas</th>
+                <th>Nama</th>
+                <th>Nama</th>
+                <th>Nama</th>
+                <th>Nama</th>
+                <th>Nama</th>
+                <th>Nama</th>
+                <th class="text-start">Kelas</th>
                 @php
                 $subjects = collect($datas)->flatMap(function ($row) {
-                return array_keys($row);
+                return array_keys((array)$row);
                 })->unique()->filter(fn($key) => !in_array($key, ['nama', 'kelas']));
                 @endphp
                 @foreach ($subjects as $subject)
@@ -30,50 +50,78 @@
             @foreach ($datas as $data)
             <tr>
                 <td class="text-start">{{ $loop->iteration }}</td>
+                <td class="text-start">{{ $data['nama'] }}</td>
                 <td>{{ $data['nama'] }}</td>
-                <td>{{ $data['kelas'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td>{{ $data['nama'] }}</td>
+                <td class="text-start">{{ $data['kelas'] }}</td>
                 @foreach ($subjects as $subject)
                 <td class="text-start">{{ $data[$subject] ?? 0 }}</td>
                 @endforeach
-                <td>
-                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#buatRapotMid">
+                <td class="text-start">
+                    <!-- Modal Trigger Buttons -->
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#buatRapotMid{{ $loop->index }}">
                         Buat Rapot Mid
                     </button>
-                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#">
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#buatRapotSemester{{ $loop->index }}">
                         Buat Rapot Semester
                     </button>
                 </td>
             </tr>
-            @endforeach
-        </tbody>
 
-        <!-- Edit Modal for each Siswa -->
-        <div class="modal fade" id="buatRapotMid" tabindex="-1" aria-labelledby="tt" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="tt">Generate Rapot Mid Semester</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                            <!-- <span aria-hidden="true">&times;</span> -->
-                        </button>
-                    </div>
-                    <form action="#" method="POST" class="m-0">
-                        @csrf
-                        @method('PUT')
+            <!-- Modal for Mid Rapot -->
+            <div class="modal fade" id="buatRapotMid{{ $loop->index }}" tabindex="-1" aria-labelledby="buatRapotMidLabel{{ $loop->index }}" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="buatRapotMidLabel{{ $loop->index }}">Rapot Mid - {{ $data['nama'] }}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
                         <div class="modal-body">
-                            <div class="form-group mb-3">
-                                <label for="nama">Komentar Rapot</label>
-                                <input type="text" name="nama" id="nama" class="form-control" value="#">
+                            @foreach ($subjects as $subject)
+                            <div class="mb-3">
+                                <label for="subject-{{ $loop->index }}" class="form-label">{{ $subject }}</label>
+                                <input type="text" class="form-control" id="subject-{{ $loop->index }}"
+                                    value="{{ $data[$subject] ?? 0 }}" readonly>
                             </div>
+                            @endforeach
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="width: 6rem">Tutup</button>
-                            <button type="submit" class="btn btn-primary" style="width: 6rem">Simpan</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <!-- Modal for Semester Rapot -->
+            <div class="modal fade" id="buatRapotSemester{{ $loop->index }}" tabindex="-1" aria-labelledby="buatRapotSemesterLabel{{ $loop->index }}" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="buatRapotSemesterLabel{{ $loop->index }}">Rapot Semester - {{ $data['nama'] }}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            @foreach ($subjects as $subject)
+                            <div class="mb-3">
+                                <label for="subject-semester-{{ $loop->index }}" class="form-label">{{ $subject }}</label>
+                                <input type="text" class="form-control" id="subject-semester-{{ $loop->index }}"
+                                    value="{{ $data[$subject] ?? 0 }}" readonly>
+                            </div>
+                            @endforeach
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </tbody>
     </table>
 </div>
 
@@ -110,6 +158,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+<script src="https://cdn.datatables.net/fixedcolumns/5.0.4/js/fixedColumns.dataTables.js"></script>
+<script src="https://cdn.datatables.net/fixedcolumns/5.0.4/js/dataTables.fixedColumns.js"></script>
 <script>
     $(document).ready(function() {
         // Cek apakah DataTable sudah diinisialisasi
@@ -119,6 +169,14 @@
 
         // Inisialisasi DataTable dengan opsi
         $('#example').DataTable({
+            fixedColumns: {
+                start: 2,
+                end: 1
+            },
+            paging: false,
+            scrollCollapse: true,
+            scrollX: true,
+            scrollY: 500,
             language: {
                 url: "{{ asset('style/js/bahasa.json') }}" // Ganti dengan path ke file bahasa Anda
             }
